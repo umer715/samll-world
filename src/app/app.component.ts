4e2ddd460308d6a3ac11b0a2d2089506df9e9ee5
umer715 <umerumer715@gmail.com>
2023-07-18 02:14:25 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: string;
+  symbol: string;
+  image: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Geoge', weight: 'Bluth', symbol: 'geogebluth@reqres.in' , image: 'geogebluth@reqres.in'},
+  {position: 2, name: 'Janet', weight: 'Weaver', symbol: 'janetweaver@reqres.in' , image: 'geogebluth@reqres.in'},
+  {position: 3, name: 'Emma', weight: 'Wong', symbol: 'emmawong@reqres.in', image: 'geogebluth@reqres.in'},
+  {position: 4, name: 'Eve', weight: 'Holt', symbol: 'eveholt@reqres.in' , image: 'geogebluth@reqres.in'},
+  {position: 5, name: 'Chales', weight: 'Morris', symbol: 'chalesmorris@reqres.in' , image: 'geogebluth@reqres.in'},
+];
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
-export class AppComponent {
-  title = 'Small-World';
+export class AppComponent{
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol' , 'image'];
+  dataSource = ELEMENT_DATA;
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule
   ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
   providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
